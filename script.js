fb32b5226bb70ea925f4cbbd9c4167e65941dd8b
Jatin Tiwary <118044955+Tiwary-ji@users.noreply.github.com>
2024-09-28 01:40:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Function to determine the relative path to 'header-footer.html'
 function getBasePath() {
     const depth = window.location.pathname.split('/').length - 2;
-    return '../'.repeat(depth) + '../Common/header-footer.html';
+    return '../'.repeat(depth) + 'ras_dsu/Common/header-footer.html';
 }
 
 // Fetch the header and footer HTML
